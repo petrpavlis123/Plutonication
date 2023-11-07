add179f40d00851126ccd5cce4dcf735a1b6825e
valentinaga1 <valentinagoa2@gmail.com>
2023-11-07 18:19:27 -0500

    Starting QR functionality and creating QR based in acces credentials uri

--- src/QRCodePopup.ts
+++ src/QRCodePopup.ts
@@ -1,13 +1,34 @@
-import QRCode from "qrcode";
+import * as qrcode from "qrcode";
+import { AccessCredentials } from "./AccesCredentials";
+import * as fs from "fs";
 
-async function generateQRCode(destinationAddress: string, amount: number): Promise<string> {
-  const data = `Address:${destinationAddress}?amount=${amount}`;
+const generateQR = async (accesCredentialUri: string | qrcode.QRCodeSegment[], outputPath: string ): Promise<void> => {
+  console.log("accesCredentialUri", accesCredentialUri);
   try {
-    const qrCodeDataUrl: string = await QRCode.toDataURL(data);
-    return qrCodeDataUrl;
-  } catch (error) {
-    throw new Error("Error generating QR code");
-  }
-}
+    const qrCodeDataUrl = await qrcode.toDataURL(accesCredentialUri);
+    const base64Data = qrCodeDataUrl.split(",")[1];
+    const buffer = Buffer.from(base64Data, "base64");
 
-export default generateQRCode;
+    fs.writeFileSync(outputPath, buffer);
+    console.log("QR code saved to", outputPath);
+  } catch (err) {
+    console.error("Error generating QR code:", err);
+  }
+};
+
+// const text = "Hello, World!";
+
+// void generateQR(text, outputPath);
+
+export { generateQR };
+
+// Using it with the accesCredentials
+const accessCredentials = new AccessCredentials(
+  "wss://plutonication-acnha.ondigitalocean.app/",
+  "1",
+  "Galaxy Logic Game",
+  "https://rostislavlitovkin.pythonanywhere.com/logo"
+);
+  
+const outputPath = "qrcode.png";
+void generateQR(accessCredentials.ToUri(), outputPath);
