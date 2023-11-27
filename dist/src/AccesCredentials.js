fcbe21c6d1e8b6d9cf0fc7a9fbcbc2eab0b59ba5
valentinaga1 <valentinagoa2@gmail.com>
2023-11-27 15:25:19 -0400

    Adding compile js files to dist folder

---
+++ dist/src/AccesCredentials.js
@@ -0,0 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Class to use correct acces credential information in the wallet
+var AccessCredentials = /** @class */ (function () {
+    function AccessCredentials(url, key, name, icon) {
+        this.url = url || this.ToUri();
+        this.key = key || AccessCredentials.GenerateKey();
+        this.name = name;
+        this.icon = icon;
+    }
+    AccessCredentials.GenerateKey = function () {
+        return Date.now().toString();
+    };
+    AccessCredentials.prototype.ToUri = function () {
+        var queryParams = [
+            "url=" + encodeURIComponent(this.url),
+            "key=" + encodeURIComponent(this.key),
+        ];
+        if (this.name != null) {
+            queryParams.push("name=" + encodeURIComponent(this.name));
+        }
+        if (this.icon != null) {
+            queryParams.push("icon=" + encodeURIComponent(this.icon));
+        }
+        return "plutonication:?" + queryParams.join("&");
+    };
+    return AccessCredentials;
+}());
+exports.AccessCredentials = AccessCredentials;
---
+++ dist/src/components/Index.js
@@ -0,0 +1,13 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// eslint-disable-next-line @typescript-eslint/quotes
+var react_1 = __importDefault(require("react"));
+var PlutonicationQrPopUp_1 = __importDefault(require("./PlutonicationQrPopUp"));
+var App = function () {
+    return (react_1.default.createElement("div", null,
+        react_1.default.createElement(PlutonicationQrPopUp_1.default, null)));
+};
+exports.default = App;
