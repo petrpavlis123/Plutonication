077bcc96a107c9f63d778deebfa62d21f57ec799
rostislavlitovkin <rostislavlitovkin@gmail.com>
2024-01-01 19:21:06 +0100

    removed test component

--- src/components/TestWebComponent.ts
+++
@@ -1,9 +0,0 @@
-export class TestWebComponent extends HTMLElement {
-  constructor() {
-    super()
-
-    this.innerHTML = "Ahojky Web componente!"
-  }
-}
-
-customElements.define("test-web-component", TestWebComponent)
