7f97164e94345194d61b9d9021593bcd94449d4c
rostislavlitovkin <rostislavlitovkin@gmail.com>
2024-01-06 19:22:42 +0100

    Valentina's change

--- example_dapp/src/App.tsx
+++ example_dapp/src/App.tsx
@@ -1,31 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { initializePlutonicationDAppClientWithModal, AccessCredentials } from '@plutonication/plutonication';
-import '@plutonication/plutonication';
-import "./MyGreeter"
-
+import './App.css'
+import {AccessCredentials, initializePlutonicationDAppClientWithModal} from "@plutonication/plutonication";
 
 function App() {
+  let account;
+
+  const initialize = async () => {
+    const accessCredentials = new AccessCredentials(
+      "wss://plutonication-acnha.ondigitalocean.app/",
+      "1",
+      "Plutonication test",
+      "https://rostislavlitovkin.pythonanywhere.com/plutowalleticonwhite"
+    );
+
+    console.log("accessCredentials:", accessCredentials.ToUri());
+
+    account = await initializePlutonicationDAppClientWithModal(
+      accessCredentials,
+      (receivedPubkey) => {
+        console.log("receivedPubkey", receivedPubkey);
+      }
+    );
+
+    console.log("injected", account);
+  };
+
+  const signMessage = async () => {
+
+    const rawMessage = {
+      address: account.address,
+      data: "0x3c42797465733e48656c6c6f20537562737472617465206d6573736167653c2f42797465733e",
+      type: "bytes",
+    }
+
+    const rawSignatureResult = await account.signer.signRaw(rawMessage)
+
+    console.log("Signature received: ", rawSignatureResult)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <button onClick={() => initializePlutonicationDAppClientWithModal(
-          new AccessCredentials(
-            "wss://plutonication-acnha.ondigitalocean.app/",
-            "100",
-            "Plutonication test",
-            "https://rostislavlitovkin.pythonanywhere.com/plutowalleticonwhite"
-          ),
-          (_) => { }
-        )}>Connect</button>
-
-        <plutonication-modal></plutonication-modal>
-
-      </header>
+    <div>
+      <plutonication-modal></plutonication-modal>
+      <button onClick={initialize}>Connect</button>
+      <button onClick={signMessage}>Sign message</button>
     </div>
   );
-}
+};
 
 export default App;
