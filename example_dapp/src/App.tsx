d8766050e5a76113475f0ab27614a8f87b6c5e0f
rostislavlitovkin <rostislavlitovkin@gmail.com>
2024-01-08 11:17:55 +0100

    Added sign payload to dApp example

--- example_dapp/src/App.tsx
+++ example_dapp/src/App.tsx
@@ -1,8 +1,12 @@
 import './App.css'
 import { AccessCredentials, initializePlutonicationDAppClientWithModal } from "@plutonication/plutonication";
+import type { SignerPayloadRaw } from "@polkadot/types/types"
+import type { Injected } from "@polkadot/extension-inject/types";
+import { ApiPromise, WsProvider } from '@polkadot/api';
 
 function App() {
-  let account:any;
+  let account: Injected;
+  let publicKey: string;
 
   const initialize = async () => {
     const accessCredentials = new AccessCredentials(
@@ -16,6 +20,7 @@ function App() {
     account = await initializePlutonicationDAppClientWithModal(
       accessCredentials,
       (receivedPubkey: string) => {
+        publicKey = receivedPubkey;
         console.log("receivedPubkey", receivedPubkey);
       }
     );
@@ -29,8 +34,13 @@ function App() {
       return;
     }
 
-    const rawMessage = {
-      address: account.address,
+    if (account.signer.signRaw == null){
+      console.warn("Signer property is not present.")
+      return;
+    }
+
+    const rawMessage: SignerPayloadRaw = {
+      address: publicKey,
       data: "0x3c42797465733e48656c6c6f20537562737472617465206d6573736167653c2f42797465733e",
       type: "bytes",
     }
@@ -40,11 +50,30 @@ function App() {
     console.log("Signature received: ", rawSignatureResult)
   };
 
+  const signBalancesTransfer = async () => {
+    if (account == null) {
+      console.warn("Account has not connected yet.")
+      return;
+    }
+
+    if (account.signer.signRaw == null){
+      console.warn("Signer property is not present.")
+      return;
+    }
+
+    const api = await ApiPromise.create({ provider: new WsProvider("wss://ws.test.azero.dev") });
+
+    api.tx.balances
+      .transfer('5C5555yEXUcmEJ5kkcCMvdZjUo7NGJiQJMS7vZXEeoMhj3VQ', 10**12)
+      .signAndSend(publicKey, { signer: account.signer }, (status: any) => { });
+  }
+
   return (
     <div>
       <plutonication-modal></plutonication-modal>
       <button onClick={initialize}>Connect</button>
       <button onClick={signMessage}>Sign message</button>
+      <button onClick={signBalancesTransfer}>Sign balances_transfer</button>
     </div>
   );
 };
