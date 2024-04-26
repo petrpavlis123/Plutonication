a38f01d7194b74f9b583e20143eb2a9569af2537
valentinaga1 <valentinagoa2@gmail.com>
2024-04-25 19:51:23 -0500

    Changing some functionalities in the web component to make it re usable in the extention

--- src/components/PlutonicationModal.ts
+++ src/components/PlutonicationModal.ts
@@ -1,4 +1,4 @@
-import * as QRCode from 'qrcode';
+import QRCode from 'qrcode';
 import { AccessCredentials } from '../AccessCredentials';
 import  { DOMAttributes }  from 'react';
 
@@ -6,9 +6,14 @@ import  { DOMAttributes }  from 'react';
  * "QR Modal used to connect the dapp to the Plutonication server.
  * @extends HTMLElement
  */
+
+const template = require('../../testing.html');
+// require('./plutonication-modal.scss');
 export class PlutonicationModal extends HTMLElement {
   private modal: HTMLElement;
   private qrImage: HTMLImageElement;
+  private plusButton: HTMLElement;
+
   // private originalWalletsContent: string;
   private additionalWallets: NodeListOf<HTMLElement>;
 
@@ -20,288 +25,54 @@ export class PlutonicationModal extends HTMLElement {
     super();
 
     const shadow = this.attachShadow({ mode: 'open' });
-
-    // document.addEventListener('DOMContentLoaded', () => {
-    //   this.setupWalletLinks();
-    //   this.setupShowMoreWallets();
-    // });
-
-    const style = document.createElement('style');
-    style.textContent = `
-    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@500&family=Lexend:wght@500&family=Libre+Franklin:wght@100&family=Open+Sans&family=Roboto&display=swap');
-
-      .plutonication__component {
-        display: none; /* Hide the modal by default */
-        position: fixed;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        justify-content: center;
-        align-items: center;
-        padding: 1rem;
-        color: white;
-        border-radius: 1rem;
-        background-color: rgba(0, 0, 0, 0.5); 
-        
-      }
-
-      .plutonication__content {
-        margin: 0;
-        padding: 1rem;
-        width: 320px;
-        background-color: #0e1110;
-        color: rgb(36, 37, 41);
-        font-size: 16px;
-        border-radius: 1rem;
-      }
-      
-      .plutonication__qr-container{
-        text-align: center;
-        border-radius: 1rem;
-        border-radius: 5px;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        padding: 0 30px;
-        border-radius: 1rem;
-      }
-      
-      .close {
-        color: #aaa;
-        font-size: 28px;
-        font-weight: bold;
-      }
-
-      .plutonication__qr-content {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-      }
-
-      .plutonication__qr-code {
-        border: 2px solid #020727;
-        background-color: #0e1110;
-        border-radius: 1rem;
-        
-      }
-
-      .scan-qr-text{
-        padding-bottom: 20px;
-      }
-
-      .close:hover,
-      .close:focus {
-        color: white;
-        text-decoration: none;
-        cursor: pointer;
-      }
-
-      .plutonication__qr-title-container {
-        display: flex;
-        flex-wrap: nowrap;
-        justify-content: center;
-        align-items: center;
-        margin-bottom: 0.5rem;
-      }
-      
-      .plutonication__qr-title {
-        margin: 0 6rem;
-      }
-
-      img {
-        border-radius: 1rem;
-      }
-
-      p, span {
-        color: white;
-        font-size: 1rem;
-        font-weight: bolder;
-        font-family: 'Roboto', sans-serif;
-        font-family: 'Inter', sans-serif;
-        font-family: 'Lexend', sans-serif;
-        font-family: 'Libre Franklin', sans-serif;
-        font-family: 'Open Sans', sans-serif;
-      }
-
-      .plutonication__wallets-container {
-        border-top: none;
-        padding: 1rem 0;
-      }
-      
-      .plutonication__wallets-container p {
-        color: white;
-      }
-
-      .plutonication__wallets-content {
-        display: grid;
-        grid-template-columns: repeat(auto-fill, minmax(70px, 1fr)); 
-        column-gap: 0.5rem;
-        row-gap: 0.5rem;
-      }
-
-      .plutonication__wallets-item,
-      .plutonication__wallets-item-plus {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        text-align: center;
-        text-decoration: none;
-        // padding-top: 0.4rem;
-        padding: 0.4rem 0;
-        // padding: 0.5rem 0.5rem;
-        
-        background-color: #393939;
-        border-radius: 10px;
-      }
-
-      .plutonication__wallets-item:hover,
-      .plutonication__wallets-item-plus:hover {
-        cursor:pointer;
-      }
-
-      .plutonication__wallets-item img {
-        // width: 50px; 
-        // height: 50px;
-        border-radius: 1rem;
-      }
-
-      .plutonication__wallets-item-description {
-        display: block;
-        text-align: center;
-        font-size: 0.65rem;
-        color: white;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-        width: 70px;
-      }
-
-      .plutonication__wallets-item-description:hover {
-        white-space: normal;
-        width: auto;
-      }
-
-      .plutonication__wallets-item:nth-child(4) {
-        padding-top: 0;
-      }
-
-      .plutonication__social-media-container{
-        display: flex;
-        justify-content: flex-end;
-        align-items: center;
-      }
-
-      .plutonication__social-media-container p {
-        color: white;
-
-      }
-
-      .plutonication__social-media-container p:hover {
-        cursor: pointer;
-      }
-    
-      .plutonication__social-media-container a {
-        margin-left: 0.5rem;
-      }
-
-
-    `;
-    shadow.appendChild(style);
-
-    // Modal element
-    this.modal = document.createElement('div');
-    this.modal.classList.add('plutonication__component');
-    this.modal.innerHTML = `
-      <div class="plutonication__content">
-        <div class="plutonication__qr-container">
-          <div class="plutonication__qr-title-container">
-            <p id="content" class="plutonication__qr-title">Plutonication</p>
-            <div>
-              <span class="close">&times;</span>
-            </div>
-          </div>
-          <div id="content" class="plutonication__qr-content">
-            <div id="qr-code" class="plutonication__qr-code"></div>
-          </div>
-        </div>
-        <div id="wallets" class="plutonication__wallets-container">
-          <p>Supported Wallets:</p>
-          <div class="plutonication__wallets-content">
-            <div href="me.rainbow" id="wallet" class="plutonication__wallets-item">
-              <img src="assets/images/rainbow-wallet.png" alt="Github" width="50" height="50">
-              <span class="plutonication__wallets-item-description">Rainbow</span>
-            </div>
-            <div href="io.metamask" id="wallet" class="plutonication__wallets-item"> 
-              <img src="assets/images/metamask-wallet.png" alt="Github" width="50" height="50">
-              <span class="plutonication__wallets-item-description">Metamask</span>
-            </div>
-            <div href="com.wallet.crypto.trustapp" id="wallet" class="plutonication__wallets-item">
-              <img src="assets/images/trust-wallet.svg" alt="Github" width="50" height="50">
-              <span class="plutonication__wallets-item-description">Trust</span>
-            </div>
-            <div id="showMoreWallets" class="plutonication__wallets-item-plus">
-              <img src="assets/images/plus-icon.svg" alt="Más" width="30" height="30">
-              <span class="plutonication__wallets-item-description"></span>
-            </div>
-            <div href="me.rainbow" id="wallet" class="plutonication__wallets-item">
-            <img src="assets/images/rainbow-wallet.png" alt="Github" width="50" height="50">
-            <span class="plutonication__wallets-item-description">Rainbow</span>
-          </div>
-          <div href="io.metamask" id="wallet" class="plutonication__wallets-item"> 
-            <img src="assets/images/metamask-wallet.png" alt="Github" width="50" height="50">
-            <span class="plutonication__wallets-item-description">Metamask</span>
-          </div>
-          <div href="com.wallet.crypto.trustapp" id="wallet" class="plutonication__wallets-item">
-            <img src="assets/images/trust-wallet.svg" alt="Github" width="50" height="50">
-            <span class="plutonication__wallets-item-description">Trust</span>
-          </div>
-
-          </div>
-        </div>
-        <div id="additional-info" class="plutonication__social-media-container">
-            <p>Docs</p>
-            <a href="https://github.com/plutonication"><img src="assets/images/github.svg" alt="Github" width="30" height="20"></a>
-            <a href="https://t.me/plutonication"><img src="assets/images/telegram.svg" alt="Telegram" width="30" height="20"></a>
-            <a href="https://plutonication.com/help"><img src="assets/images/question-mark.svg" alt="Help" width="30" height="20"></a>
-        </div>
-      </div>
-      `;
-    shadow.appendChild(this.modal);
-
-    //QR code element
-    this.qrImage = document.createElement('img');
-    this.qrImage.setAttribute('alt', 'QR Code');
-    this.qrImage.style.display = 'none';
-    this.modal.querySelector('.plutonication__qr-code')?.appendChild(this.qrImage);
-    // this.originalWalletsContent = '';
-
-    // Close the modal
-    const closeButton = this.modal.querySelector('.close');
-    closeButton?.addEventListener('click', () => {
-      this.closeModal();
-    });
-    
-
-    // Show more wallets
-    // const showMoreWalletsButton = this.modal.querySelector('#showMoreWallets');
-    // if (showMoreWalletsButton) {
-    //   showMoreWalletsButton.addEventListener('click', () => {
-    //     this.setupShowMoreWallets();
-    //   });
-    // }
-
-    // // Show wallets download button
-    // const selectedWallet = this.modal.querySelector('#wallet');
-    // console.log("selectedWallet", selectedWallet);
-    // if (selectedWallet) {
-    //   selectedWallet.addEventListener('click', () => {
-    //     console.log("selecting wallet...");
-    //     this.setupWalletLinks();
-    //   });
-    // }
+    shadow.innerHTML = template.default;
   }
+
+  connectedCallback() {
+    const accessCredentials = new AccessCredentials(
+        "wss://plutonication-acnha.ondigitalocean.app/",
+        "Plutonication test",
+        "https://rostislavlitovkin.pythonanywhere.com/plutowalleticonwhite"
+    );
+
+    this.generateQRCode(accessCredentials.ToUri());
+    this.plusButton = this.shadowRoot.getElementById('showMoreWallets');
+    console.log("this.plusButton",this.plusButton);
+    this.plusButton.addEventListener('click', () => {
+      console.log("MOSTRANDO WALLETS");
+      this.showMoreWallets();
+    });
+  }
+
+  // Function to generate the QR code with the accesCredentials
+  async generateQRCode(inputText: string) {
+    // const inputText = 'plutonication:?url=wss%3A%2F%2Fplutonication-acnha.ondigitalocean.app%2F&key=1710194226878&name=Plutonication%20test&icon=https%3A%2F%2Frostislavlitovkin.pythonanywhere.com%2Fplutowalleticonwhite'; 
+    console.log("Generating QR Code");
+    const qrCodeContainer = this.shadowRoot.getElementById('qr-code');
+
+    try {
+        const qrCodeDataURL = await QRCode.toDataURL(inputText);
+        const qrCodeImage = document.createElement('img');
+        qrCodeImage.src = qrCodeDataURL;
+        console.log("qrCodeContainer", qrCodeContainer);
+        if (qrCodeContainer) {
+            qrCodeContainer.innerHTML = '';
+            qrCodeContainer.appendChild(qrCodeImage);
+        }
+    } catch (error) {
+        console.error('Something went wrong generating the QR code:', error);
+    }
+}
+
+showMoreWallets() {
+  this.plusButton.style.display = 'none';
+  const moreWalletsItem = this.shadowRoot.querySelectorAll('.plutonication__wallets-item-hidden');
+  moreWalletsItem.forEach(wallet => {
+    wallet.classList.remove('plutonication__wallets-item-hidden');
+});
+}
+
+// Function to show more wallets if there exists more than four
 
   /**
    * Opens the modal and displays the QR code generated with the access credentials information.
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,18 @@
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require("path");
+
+const entryPoints = {
+  main: [
+      path.resolve(__dirname, './', 'index.ts'),
+      path.resolve(__dirname, './', 'plutonication-modal.scss')
+  ],
+};
 
 module.exports = {
   mode: "production",
-  entry: './index.ts',
+  entry: entryPoints,
   output: {
     path: path.resolve(__dirname, 'lib'),
     filename: 'plutonication.js',
@@ -15,32 +25,61 @@ module.exports = {
   },
   module: {
     rules: [
+      // {
+      //   test: /\.(ts|tsx)$/,
+      //   exclude: /node_modules/,
+      //   use: {
+      //     loader: 'babel-loader',
+      //     options: {
+      //       presets: ['@babel/preset-env', '@babel/preset-typescript'],
+      //       plugins: ['@babel/plugin-proposal-export-default-from'],
+      //     },
+      //   },
+      // },
       {
-        test: /\.(ts|tsx)$/,
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-typescript'],
-            plugins: ['@babel/plugin-proposal-export-default-from'],
-          },
-        },
-      },
+    },
+      // {
+      //   test: /\.(js|jsx)$/,
+      //   exclude: /node_modules/,
+      //   use: {
+      //     loader: 'babel-loader',
+      //     options: {
+      //       presets: ['@babel/preset-env'],
+      //     },
+      //   },
+      // },
+      // {
+      //   test: /\.css$/,
+      //   use: ['style-loader', 'css-loader'],
+      //   include: path.resolve(__dirname, '/'), 
+      // },
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
+        test: /\.(sa|sc)ss$/,
+        use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader'
+        ]
+    },
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-        include: path.resolve(__dirname, '/'), 
+        test: /\.html$/,
+        exclude: /node_modules/, // Excluimos los archivos HTML de ser procesados como módulos
+        use: 'html-loader', // No necesitamos este loader realmente, solo lo usamos para que Webpack no arroje un error
       },
     ],
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: '.', to: 'images', context: 'images' }],
+      
+  }),
+    new MiniCssExtractPlugin({
+        filename: '[name].css',
+    }),
+
+]
+  // plugins: [new HtmlWebpackPlugin({ template: 'testing.html' })],
 };
