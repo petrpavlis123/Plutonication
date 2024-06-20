1c5277fbf1290ca69e7ff3e2709c5345c91e2892
Rostislav Litovkin <rostislavlitovkin@gmail.com>
2024-06-20 14:52:29 +0200

    Code cleanup

--- packages/npm-package/webpack.config.js
+++ packages/npm-package/webpack.config.js
@@ -40,14 +40,6 @@ module.exports = {
         },
       },
       {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader',
-        ],
-      },
-      {
         test: /\.html$/,
         exclude: /node_modules/,
         use: 'html-loader',
